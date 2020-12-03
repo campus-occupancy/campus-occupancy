40138da9af7ce320b9f65dd59674df9b98b60e3c
CalianaFortin <70739998+CalianaFortin@users.noreply.github.com>
2020-12-03 13:53:24 -1000

    Complete the density collection

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -64,13 +64,13 @@ if (Meteor.users.find().count() === 0) {
  * This approach allows you to initialize your system with large amounts of data.
  * Note that settings.development.json is limited to 64,000 characters.
  * We use the "Assets" capability in Meteor.
- * For more info on assets, see https://docs.meteor.com/api/assets.html
+ * For more info on assets, see
  * User count check is to make sure we don't load the file twice, which would generate errors due to duplicate info.
  */
 if ((Meteor.settings.loadAssetsFile) && (Meteor.users.find().count() < 7)) {
-  const assetsFileName = 'data.json';
+  const assetsFileName = 'density.json';
   console.log(`Loading data from private/${assetsFileName}`);
   const jsonData = JSON.parse(Assets.getText(assetsFileName));
-  jsonData.profiles.map(profile => addProfile(profile));
-  jsonData.projects.map(project => addProject(project));
+  // jsonData.profiles.map(profile => addProfile(profile));
+  // jsonData.projects.map(project => addProject(project));
 }
