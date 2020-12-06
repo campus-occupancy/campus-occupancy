0343d8cd3ab177ffb1619b3045bea7b502cc5243
CalianaFortin <70739998+CalianaFortin@users.noreply.github.com>
2020-12-05 14:45:58 -1000

    added the collection

---
+++ app/imports/api/densityData/Datas.js
@@ -0,0 +1,26 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+import { Tracker } from 'meteor/tracker';
+
+/** Encapsulates state and variable values for this collection. */
+class DatasCollection {
+  constructor() {
+    // The name of this collection.
+    this.name = 'ProfilesCollection';
+    // Define the Mongo collection.
+    this.collection = new Mongo.Collection(this.name);
+    // Define the structure of each document in the collection.
+    this.schema = new SimpleSchema({
+      dateTime: { type: String, index: true, unique: true },
+      Unique: { type: String, optional: true },
+      Building: { type: String, optional: true },
+    }, { tracker: Tracker });
+    // Ensure collection documents obey schema.
+    this.collection.attachSchema(this.schema);
+    // Define names for publications and subscriptions
+    this.userPublicationName = `${this.name}.publication.user`;
+    this.adminPublicationName = `${this.name}.publication.admin`;
+  }
+}
+
+export const Profiles = new DatasCollection();
