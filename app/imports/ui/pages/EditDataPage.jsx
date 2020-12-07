9515f019bb6a749b539395bc0448929ff5a76af5
buccatm <70668682+buccatm@users.noreply.github.com>
2020-12-07 11:11:52 -1000

    this contains the vs upload file

--- app/imports/ui/pages/EditDataPage.jsx
+++ app/imports/ui/pages/EditDataPage.jsx
@@ -1,8 +1,40 @@
 import React from 'react';
 import { Container, Grid, Header, Button } from 'semantic-ui-react';
+import Papa from 'papaparse';
+import { Datas } from '../../api/dataDensity/Datas';
+
 
 /** Renders a color-blocked static landing page. */
 class EditDataPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      csvfile: undefined,
+    };
+    this.updateData = this.updateData.bind(this);
+  }
+
+  handleChange = event => {
+    this.setState({
+      csvfile: event.target.files[0],
+    });
+    console.log(this.state);
+  };
+
+  importCSV = () => {
+    const { csvfile } = this.state;
+    Papa.parse(csvfile, {
+      complete: this.updateData,
+      header: true,
+    });
+  };
+
+  updateData(result) {
+    const data = result.data;
+    console.log(data);
+    data.map(nums => Datas.insert(nums));
+  }
+
   render() {
     return (
         <div id="editDatePage">
@@ -22,7 +54,10 @@ class EditDataPage extends React.Component {
                 <Button color='black' as="label" htmlFor="file" type="button">
                   Upload
                 </Button>
-                <input type="file" id="file" style={{ display: 'hidden' }} onChange={this.onChange} />
+                <input type="file" id="file" ref={input => {
+                  this.filesInput = input;
+                }}style={{ display: 'hidden' }} onChange={this.handleChange} />
+                <Button onClick={this.importCSV}>Import Now!</Button>
               </Grid.Column>
               <Grid.Column>
                 <Button color='black'>Generate Map</Button>
