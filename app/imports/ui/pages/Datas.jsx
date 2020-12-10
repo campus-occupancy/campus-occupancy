b529e6471f0aab69f91a6603ac9cde0e7aa2f58a
CalianaFortin <70739998+CalianaFortin@users.noreply.github.com>
2020-12-09 18:49:37 -1000

    Reverted the changes so it does display the page
    The covidmap component does not work

--- app/imports/ui/pages/Datas.jsx
+++ app/imports/ui/pages/Datas.jsx
@@ -20,7 +20,7 @@ class ListData extends React.Component {
   renderPage() {
     return (
         <Container>
-          <div><Covid19Map/></div>
+          <div><Covid19/></div>
           <Header as="h2" textAlign="center">Occupancy Data</Header>
           <Table celled>
             <Table.Header>
