0061b32dfd8492b6ef9f5f3472402cef2af4ede5
CalianaFortin <70739998+CalianaFortin@users.noreply.github.com>
2020-12-15 06:02:55 -1000

    Trying to get the collections to work

--- app/imports/ui/components/Covid19Map.jsx
+++ app/imports/ui/components/Covid19Map.jsx
@@ -3,6 +3,7 @@ import { Meteor } from 'meteor/meteor';
 import { Loader } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
+import papa from 'papaparse';
 import { Datas } from '../../api/dataDensity/Datas';
 import { features } from '../../../data/campusmap.json';
 import legendItems from '../../../entities/LegendItems';
@@ -12,70 +13,94 @@ import CovidSlider from '../../api/Covid19/covidSlider';
 import CovidMap from '../../api/Covid19/CovidMap';
 import Legend from '../../api/Covid19/Legend';
 
-/** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
+const Covid19 = () => {
+  const [buildings, setBuildings] = useState([]); // possibly have to change this line
+  const legendItemsInReverse = [...legendItems].reverse();
+
+  const load = (setState) => {
+    this.setState = setState;
+    const covid19Data = this.props.data;
+    this.processCovidData(covid19Data);
+
+  };
+
+  useEffect(load, []);
+  return (
+      <div>
+        {buildings.length === 0 ? <Loading/> : <div>
+          <div className='container'>
+            <div className="text-block"><CovidSlider/></div>
+          </div>
+          <CovidMap/>
+
+        </div>
+        }
+      </div>);
+};
+
  class Covid19Map extends React.Component {
+   /* constructor(props) {
+     super(props);
+     // this.state = {  [buildings, setBuildings] };
+   }*/
+
+   setState = null;
+
+   load = (setState) => {
+     this.setState = setState;
+     const covid19Data = this.props.data;
+     this.processCovidData(covid19Data);
+
+   };
+
+   #processCovidData = (covidBuildings) => {
+     for (let i = 0; i < features.length; i++) {
+       const building = features[i];
+       // eslint-disable-next-line no-shadow,no-loop-func
+       const covidBuilding = covidBuildings.find((covidBuilding) => building.properties.Building === covidBuilding.Building);
+
+       building.properties.confirmed = 0;
+       building.properties.confirmedText = '0';
+
+       if (covidBuilding != null) {
+         const confirmed = covidBuilding['Unique Clients'];
+         // console.log(covidBuilding['Unique Clients']);
+         building.properties.confirmed = confirmed;
+         building.properties.confirmedText = confirmed;
+       }
+       this.setBuildingColor(building);
+     }
+
+     this.setState(features);
+
+   }
+
+   #setBuildingColor = (building) => {
+     const legendItem = legendItems.find((item) => item.isFor(building.properties.confirmed));
+
+     // eslint-disable-next-line no-param-reassign
+     if (legendItem != null) building.properties.color = legendItem.color;
+   }
 
   render() {
     return (this.props.ready) ? this.renderPage() : <Loader>Getting data</Loader>;
   }
 
-  covidBuildings = this.props.datas;
-
-  #processCovidData = (covidBuildings) => {
-    for (let i = 0; i < features.length; i++) {
-      const building = features[i];
-      // eslint-disable-next-line no-shadow,no-loop-func
-      const covidBuilding = covidBuildings.find((covidBuilding) => building.properties.Building === covidBuilding.Building);
-
-      building.properties.confirmed = 0;
-      building.properties.confirmedText = '0';
-
-      if (covidBuilding != null) {
-        const confirmed = covidBuilding['Unique Clients'];
-        // console.log(covidBuilding['Unique Clients']);
-        building.properties.confirmed = confirmed;
-        building.properties.confirmedText = confirmed;
-      }
-      this.#setBuildingColor(building);
-    }
-
-    this.setState(features);
-
-  };
-
-  #setBuildingColor = (building) => {
-    const legendItem = legendItems.find((item) => item.isFor(building.properties.confirmed));
-
-    // eslint-disable-next-line no-param-reassign
-    if (legendItem != null) building.properties.color = legendItem.color;
-  };
-
-  Covid19 = () => {
-    const [buildings, setBuildings] = useState([]); // possibly have to change this line
-
-    const load = () => { // have to change this line
-      const loadBuildingsTask = new LoadBuildingsTask();
-      loadBuildingsTask.load(setBuildings);
-    };
-
-    useEffect(load, []);
-  };
-
   renderPage() {
-    const legendItemsInReverse = [...legendItems].reverse();
-    const [buildings, setBuildings] = useState([]); // possibly have to change this line
+     // const mapData = processCovidData(this.props.datas);
+     const legendItemsInReverse = [...legendItems].reverse();
 
     return (
         <div>
-          {buildings.length === 0 ? <Loading/> : <div>
+           <div>
             <div className='container'>
               <div className="text-block"><CovidSlider/></div>
             </div>
             <CovidMap/>
             <Legend legendItems={legendItemsInReverse}/>
           </div>
-          }
         </div>
+        // <div> THIS IS A MAP</div>
 
     );
   }
